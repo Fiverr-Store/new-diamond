1bf2273be0484eead2233cf79985704e5c653cda
ali-dev-code <ali.asgr514@gmail.com>
2023-01-19 17:27:27 +0500

    uploaded

---
+++ assets/erc165.d8223f31.js
@@ -0,0 +1,2 @@
+var t=[{inputs:[],payable:!1,stateMutability:"nonpayable",type:"constructor"},{constant:!0,inputs:[{internalType:"bytes4",name:"interfaceId",type:"bytes4"}],name:"supportsInterface",outputs:[{internalType:"bool",name:"",type:"bool"}],payable:!1,stateMutability:"view",type:"function"}];export{t as default};
+//# sourceMappingURL=erc165.d8223f31.js.map
